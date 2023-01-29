4877f311243b294f6b9b824bf578c1d7bdaefa7d
Barry Tielkes <barry@instantmagazine.com>
2023-01-29 01:09:58 +0100

    changing folder works

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 
-const getFiles = async () => {
-	const dir = await fs.promises.readdir('./');
-	const dirItems = dir.map(async (file) => {
-		return new Promise((resolve, reject) => {
-			fs.stat(`./${file}`, (err, stats) => {
-				if (err) {
-					resolve(err);
-					throw err;
-				}
-				resolve({ label: file, type: stats.isFile() ? 'file' : 'dir' });
+const gotoDirectory = (folder) => {
+	const getFiles = async () => {
+		const dir = await fs.promises.readdir(folder);
+		const dirItems = dir.map(async (file) => {
+			return new Promise((resolve, reject) => {
+				fs.stat(`${folder}${file}`, (err, stats) => {
+					if (err) {
+						return resolve({ label: file, type: 'unreadable' });
+					}
+					resolve({ label: file, type: stats.isFile() ? 'file' : 'dir' });
+				});
 			});
 		});
-	});
-	return Promise.all(dirItems);
-};
+		return Promise.all(dirItems);
+	};
 
-getFiles().then((dirItems) => {
-	const prompt = inquirer.createPromptModule({});
-	prompt({
-		type: 'list',
-		name: 'decision',
-		choices: dirItems.map((dirItem) => {
-			const isDir = dirItem.type === 'dir';
-			return { name: isDir ? `[${dirItem.label}]` : dirItem.label, value: dirItem };
-		}),
-	})
-		.then((answer) => {
-			console.log('answer', answer);
+	getFiles().then((dirItems) => {
+		const prompt = inquirer.createPromptModule({});
+		prompt({
+			type: 'list',
+			name: 'decision',
+			choices: [
+				{ name: '[../]', value: { label: '../', type: 'dir' } },
+				...dirItems.map((dirItem) => {
+					const isDir = dirItem.type === 'dir';
+					return { name: isDir ? `[${dirItem.label}]` : dirItem.label, value: dirItem };
+				}),
+			],
 		})
-		.catch((error) => {
-			if (error.isTtyError) {
-				console.log('error', error);
-				// Prompt couldn't be rendered in the current environment
-			} else {
-				console.log('Something else went wrong');
-				// Something else went wrong
-			}
-		});
-});
+			.then(({ decision }) => {
+				if (decision.type === 'dir') {
+					gotoDirectory(`${folder}${decision.label}/`);
+				}
+			})
+			.catch((error) => {
+				if (error.isTtyError) {
+					console.log('error', error);
+					// Prompt couldn't be rendered in the current environment
+				} else {
+					console.log('Something else went wrong');
+					// Something else went wrong
+				}
+			});
+	});
+};
+gotoDirectory('./');
