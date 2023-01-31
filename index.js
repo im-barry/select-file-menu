3ad6837a1af522f88c5279a1912623cf6de084de
Barry Tielkes <barry@instantmagazine.com>
2023-01-31 23:12:20 +0100

    WIP, prepare to move to github

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import inquirer from 'inquirer';
 import process from 'process';
-import { spawn, exec } from 'node:child_process';
+// import shelljs from 'shelljs';
+import { spawn, exec, execSync } from 'node:child_process';
 import fs from 'fs';
 
 const gotoDirectory = (folder) => {
@@ -37,18 +38,42 @@ const gotoDirectory = (folder) => {
 					gotoDirectory(`${folder}${decision.label}/`);
 				} else {
 					console.log('a decision was made: ', decision);
+					console.log('a decision was made2: ', process.argv[2], folder + decision.label);
 					//process.argv[2]
 					if (process.argv[2]) {
-						console.log('execute', process.argv[2], ' on: ', `${folder}${decision.label}`);
+						console.log('call', process.argv[2], ' with ', `${folder}${decision.label}`);
 
-						// this works for nano:
-						const command = spawn(process.argv[2], [`${folder}${decision.label}`], {
-							stdio: 'inherit',
-							detached: true,
-						});
-						command.stdout.on('data', (output) => {
-							console.log('output');
-						});
+						// console.log('execute', process.argv[2], ' on: ', `${folder}${decision.label}`, shelljs);
+						// shelljs.echo('my cache dir is', shelljs.env);
+						// shelljs.exec(
+						// 	`shopt -s expand_aliases \n source ~/.bashrc \n alias ${process.argv[2]} \n ${process.argv[2]} ${
+						// 		folder + decision.label
+						// 	}`,
+						// 	{ shell: '/bin/bash' }
+						// );
+						// execSync(`shopt -s expand_aliases \n source ~/.bashrc \n ${process.argv[2]} ${folder + decision.label}`, {
+						// 	shell: '/bin/bash',
+						// }); //.toString();
+						// shelljs.exec(`source ~/.bashrc; ${process.argv[2]}`, { shell: '/bin/bash' });
+						// shelljs.exec(
+						// 	process.argv[2],
+						// 	{ shell: '/bin/bash' } //, stdio: 'inherit', detached: true },
+						// function (error, stdout, stderr) {
+						// 	console.log('stdout: ' + stdout);
+						// 	console.log('stderr: ' + stderr);
+						// 	if (error !== null) {
+						// 		console.log('exec error: ' + error);
+						// 	}
+						// }
+						// );
+						// // this works for nano:
+						// const command = spawn(process.argv[2], [`${folder}${decision.label}`], {
+						// 	stdio: 'inherit',
+						// 	detached: true,
+						// });
+						// command.stdout.on('data', (output) => {
+						// 	console.log('output');
+						// });
 						// exec(process.argv[2], (err, output) => {
 						// 	if (err) {
 						// 		return console.log('error', err);
