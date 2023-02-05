3f01fbe21b9a48c10642eb7dbfb7f94b7446e212
Barry Tielkes <barry@instantmagazine.com>
2023-02-05 02:55:21 +0100

    got it fully working, needed to export the bash function. Also made it possible to add additional arguments to pass onto the bash function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import inquirer from 'inquirer';
 import process from 'process';
-// import shelljs from 'shelljs';
+import shelljs from 'shelljs';
 import { spawn, exec, execSync } from 'node:child_process';
 import fs from 'fs';
 
@@ -11,6 +11,7 @@ const gotoDirectory = (folder) => {
 			return new Promise((resolve, reject) => {
 				fs.stat(`${folder}${file}`, (err, stats) => {
 					if (err) {
+						console.log('err', err);
 						return resolve({ label: file, type: 'unreadable' });
 					}
 					resolve({ label: file, type: stats.isFile() ? 'file' : 'dir' });
@@ -26,7 +27,7 @@ const gotoDirectory = (folder) => {
 			type: 'list',
 			name: 'decision',
 			choices: [
-				{ name: '[../]', value: { label: '../', type: 'dir' } },
+				{ name: '[../]', value: { label: '..', type: 'dir' } },
 				...dirItems.map((dirItem) => {
 					const isDir = dirItem.type === 'dir';
 					return { name: isDir ? `[${dirItem.label}]` : dirItem.label, value: dirItem };
@@ -37,49 +38,22 @@ const gotoDirectory = (folder) => {
 				if (decision.type === 'dir') {
 					gotoDirectory(`${folder}${decision.label}/`);
 				} else {
-					console.log('a decision was made: ', decision);
-					console.log('a decision was made2: ', process.argv[2], folder + decision.label);
-					//process.argv[2]
 					if (process.argv[2]) {
-						console.log('call', process.argv[2], ' with ', `${folder}${decision.label}`);
+						let command = `source ~/.bashrc; ${process.argv[2]} "${folder}" "${decision.label}"`;
+						let counter = 3;
+						while (counter < 10) {
+							if (process.argv[counter]) {
+								command += ` ${process.argv[counter]}`;
+							} else {
+								break;
+							}
+							counter++;
+						}
+						console.log('execute command:', command);
 
-						// console.log('execute', process.argv[2], ' on: ', `${folder}${decision.label}`, shelljs);
-						// shelljs.echo('my cache dir is', shelljs.env);
-						// shelljs.exec(
-						// 	`shopt -s expand_aliases \n source ~/.bashrc \n alias ${process.argv[2]} \n ${process.argv[2]} ${
-						// 		folder + decision.label
-						// 	}`,
-						// 	{ shell: '/bin/bash' }
-						// );
-						// execSync(`shopt -s expand_aliases \n source ~/.bashrc \n ${process.argv[2]} ${folder + decision.label}`, {
-						// 	shell: '/bin/bash',
-						// }); //.toString();
-						// shelljs.exec(`source ~/.bashrc; ${process.argv[2]}`, { shell: '/bin/bash' });
-						// shelljs.exec(
-						// 	process.argv[2],
-						// 	{ shell: '/bin/bash' } //, stdio: 'inherit', detached: true },
-						// function (error, stdout, stderr) {
-						// 	console.log('stdout: ' + stdout);
-						// 	console.log('stderr: ' + stderr);
-						// 	if (error !== null) {
-						// 		console.log('exec error: ' + error);
-						// 	}
-						// }
-						// );
-						// // this works for nano:
-						// const command = spawn(process.argv[2], [`${folder}${decision.label}`], {
-						// 	stdio: 'inherit',
-						// 	detached: true,
-						// });
-						// command.stdout.on('data', (output) => {
-						// 	console.log('output');
-						// });
-						// exec(process.argv[2], (err, output) => {
-						// 	if (err) {
-						// 		return console.log('error', err);
-						// 	}
-						// 	console.log('output');
-						// });
+						shelljs.exec(command, {
+							shell: '/bin/bash',
+						});
 					}
 				}
 			})
